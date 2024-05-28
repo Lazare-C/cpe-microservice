c4b93b7003ba4be2bcfca6ed1938c3dfe911c962
Mathis <malekmathis2@gmail.com>
2024-05-28 11:56:15 +0200

    add account unit testing

---
+++ atelier3/Projet/account/src/test/java/fr/dreamteam/AccountApplicationTests.java
@@ -0,0 +1,100 @@
+package fr.dreamteam;
+
+import fr.dreamteam.account.bo.UserBo;
+import fr.dreamteam.account.exception.LoginException;
+import fr.dreamteam.account.mapper.UserMapper;
+import fr.dreamteam.account.repository.UserRepository;
+import fr.dreamteam.account.service.AuthService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountApplicationTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+
+    @InjectMocks
+    private AuthService authService;
+    
+
+    @Test
+    public void registerUser_UserAlreadyExists_ThrowsLoginException() {
+        String username = "test";
+        String password = "test";
+        UserBo existingUser = new UserBo(username, password);
+        when(userRepository.findByUsername(username)).thenReturn(existingUser);
+
+        assertThrows(LoginException.class, () -> authService.registerUser(username, password));
+        try{
+            authService.registerUser(username, password);
+        }catch(LoginException e){
+            assertTrue(e.getMessage().equals("User already exists"));
+        }
+    }
+
+    @Test
+    public void registerUser_NewUser_Success() {
+        String username = "existingUser";
+        String password = "password";
+        
+        when(userRepository.findByUsername(username)).thenReturn(null);
+        authService.registerUser(username, password);
+        UserBo existingUser = new UserBo(username, password);
+
+        when(userRepository.findByUsername(username)).thenReturn(existingUser);
+        UserBo actual = userRepository.findByUsername(username);
+
+        assertTrue(EqualsBuilder.reflectionEquals(existingUser,actual));
+    }
+
+    @Test
+    public void Login_User_Does_Not_Exist_ThrowsLoginException() {
+        String username = "doesNotExist";
+        String password = "doesNotExist";
+        
+        assertThrows(LoginException.class, () -> authService.loginUser(username, password));
+        try{
+            authService.loginUser(username, password);
+        }catch(LoginException e){
+            assertTrue(e.getMessage().equals("User does not exist"));
+        }
+    }
+
+    @Test
+    public void Login_Wrong_Password_ThrowsLoginException() {
+        String username = "user";
+        String password = "Password";
+        String wrongPassword = "wrongPassword";
+
+        authService.registerUser(username, password);
+        UserBo existingUser = new UserBo(username, password);
+        when(userRepository.findByUsername(username)).thenReturn(existingUser);
+
+        assertThrows(LoginException.class, () -> authService.loginUser(username, wrongPassword));
+        try{
+            authService.loginUser(username, wrongPassword);
+        }catch(LoginException e){
+            assertTrue(e.getMessage().equals("Incorrect password"));
+        }
+    }
+
+}
